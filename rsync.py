0a3387aad15c6d771f44b6f5328c2e285e2d99b5
baodongle <baodongle@gmail.com>
2019-02-27 21:25:57 +0700

    add argparse library

---
+++ rsync.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="""rsync - a fast, versatile,
+                                    remote (and local) file-copying tool""")
